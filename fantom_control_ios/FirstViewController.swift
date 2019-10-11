34a8bb29703d3e67fe8c2dd23485710787c92e64
Roy Zhao <ziyi_zhao@sfu.ca>
2019-10-11 14:22:22 -0700

    Initial Commit

---
+++ fantom_control_ios/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  fantom_control_ios
+//
+//  Created by Roy Zhao on 2019-10-11.
+//  Copyright © 2019 Ziyi Zhao. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ fantom_control_ios/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  fantom_control_ios
+//
+//  Created by Roy Zhao on 2019-10-11.
+//  Copyright © 2019 Ziyi Zhao. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
